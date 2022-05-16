28268635f346a94c290df363034fa06f7151a652
consul2022 <millennium.park12@yandex.ru>
2022-05-16 17:26:20 +0300

    final commit

--- DataBase.py
+++ DataBase.py
@@ -1,5 +1,5 @@
 from sqlite3 import *
-
+from difflib import *
 # fetchone получить один из запроса пользователя с базы данных
 # fetchall получить всё из запроса пользователя с базы данных
 class DataBase:
@@ -20,3 +20,19 @@ class DataBase:
     def select_video_by_id(self, id):
         requests_id = self.cur.execute(f"SELECT Video FROM Films WHERE id = {id}").fetchone()
         return requests_id
+
+    def select_possible_films(self, name):
+        all_names = self.cur.execute(f"SELECT id, Name FROM Films").fetchall()
+        good_films = []
+        for id, name_film in all_names:
+            mather = SequenceMatcher(a=name,b=name_film)
+            if mather.ratio() > 0.7:
+                good_films.append(name_film)
+        # requests_film = []
+        # for i in good_films:
+        #     requests_film.append(self.cur.execute(f"SELECT Poster, Name, Genre, Description, Video From Films Where id = {i} ").fetchone())
+
+        return good_films
+
+
+
